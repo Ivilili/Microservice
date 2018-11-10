e6811a93c79a2e942e914c1efe9c7b131dd84704
Ivana Livaja <ivana.livaja@hotmail.com>
2018-11-10 14:13:27 +0100

    changed index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,49 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index');
+});
+
+router.get('/:time', function(req, res) {
+	function unixToNatural(unix) {
+		let date = new Date(unix * 1000);
+		let months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		];
+
+		let month = months[date.getMonth()];
+		let day = date.getDate();
+		let year = date.getFullYear();
+
+		let result = month + ' ' + day + ', ' + year;
+		return result;
+	}
+
+	if (!isNaN(req.params.time)) {
+		let result = unixToNatural(req.params.time);
+		let data = { unix: req.params.time, natural: result };
+		res.json(data);
+	} else {
+		let natural = new Date(req.params.time);
+		if (!isNaN(natural)) {
+			let unix = natural / 1000;
+			let data = { unix: unix, natural: req.params.time };
+			res.json(data);
+		} else {
+			res.json({ error: 'Invalid Date' });
+		}
+	}
 });
 
 module.exports = router;
